9afa995d3af1eb2e8d775f16ec6e7b7b852758f8
ashunshah <ashunshah@outlook.com>
2017-04-10 16:54:27 -0400

    Small update

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs')
+const port =process.env.PORT || 3000;
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
 
 app.use((req,res,next) => {
@@ -25,6 +25,7 @@ app.use((req,res,next) => {
   res.render('404.hbs');
 })
 
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
@@ -54,6 +55,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
